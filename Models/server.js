87d6ad2064936aee3ec3bcce08ee9ee3b45815fd
elderopedia <loicmagny60@gmail.com>
2022-09-27 13:34:46 +0200

    init

---
+++ Models/server.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const app = express();
+const fs = require('fs').promises;
+const { resolve } = require('path');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+
+
+app.get("/", function (req, res) {
+    res.render('home')
+});
+
+app.get("/products", (req, res) => {
+
+    readTextFile("Datas/products.json", function (text) {
+        res.json(JSON.parse(text));
+    });
+})
+
+
+app.listen('8000', 'localhost', () => {
+    console.log('server start')
+})
