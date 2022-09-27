f5387bc3831c474b4d88c470500010ff27f5dbf8
elderopedia <loicmagny60@gmail.com>
2022-09-27 15:08:04 +0200

    model user et product

---
+++ Models/ProductsModel.js
@@ -0,0 +1,82 @@
+class Products {
+
+    constructor(id, name, description, stars, price, img, water, light, temperature, humidity) {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.stars = stars
+        this.price = price
+        this.img = img
+        this.water = water
+        this.light = light
+        this.temperature = temperature
+        this.humidity = humidity
+    }
+
+    get id() {
+        return this.id
+    }
+    get name() {
+        return this.name
+    }
+    get description() {
+        return this.description
+    }
+    get stars() {
+        return this.stars
+    }
+    get price() {
+        return this.price
+    }
+    get img() {
+        return this.img
+    }
+    get water() {
+        return this.water
+    }
+    get light() {
+        return this.light
+    }
+    get temperature() {
+        return this.temperature
+    }
+    get humidity() {
+        return this.humidity
+    }
+    set id(value) {
+        return this.id = value
+    }
+    set name(value) {
+        return this.name = value
+    }
+    set description(value) {
+        return this.description = value
+    }
+    set stars(value) {
+        return this.stars = value
+    }
+    set price(value) {
+        return this.price = value
+    }
+    set img(value) {
+        return this.img = value
+    }
+    set water(value) {
+        return this.water = value
+    }
+    set light(value) {
+        return this.light = value
+    }
+    set temperature(value) {
+        return this.temperature = value
+    }
+    set humidity(value) {
+        return this.humidity = value
+    }
+
+
+
+
+
+
+}
---
+++ Models/UserModel.js
@@ -0,0 +1,68 @@
+class User {
+    constructor(id, username, password, email, firstname, lastname, adress, phone) {
+        this.id = id
+        this.username = username
+        this.pasword = password
+        this.email = email
+        this.firstname = firstname
+        this.lastname = lastname
+        this.adress = adress
+        this.phone = phone
+    }
+
+    get id() {
+        return this.id
+    }
+    set id(value) {
+        return this.id = value
+    }
+
+    get username() {
+        return this.username
+    }
+    set username(value) {
+        return this.username = value
+    }
+
+    get password() {
+        return this.password
+    }
+    set password(value) {
+        return this.password = value
+    }
+
+    get email() {
+        return this.password
+    }
+    set email(value) {
+        return this.email = value
+    }
+
+    get firstname() {
+        return this.firstname
+    }
+    set firstname(value) {
+        return this.firstname = value
+    }
+
+    get lastname() {
+        return this.lastname
+    }
+    set lastname(value) {
+        return this.lastname = value
+    }
+
+    get adress() {
+        return this.adress
+    }
+    set adress(value) {
+        return this.adress = value
+    }
+
+    get phone() {
+        return this.phone
+    }
+    set phone() {
+        return this.phone = value
+    }
+}
