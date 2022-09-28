6e1903374b3215e4ca179e9c6ccd9589416791d6
Antoine <agaudry.lamanu@gmail.com>
2022-09-28 11:47:29 +0200

    antoine

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//Routes
 app.get('/', function(req, res) {
 	fs.readFile('Datas/products.json', (err, data) => {
 		if (err) throw err;
@@ -16,8 +17,21 @@ app.get('/', function(req, res) {
 		});
 	});
 });
+
 app.get("/nursery", function (req, res) {
     res.render('nursery')
+});
+app.get("/cart", function (req, res) {
+    res.render('cart')
+});
+
+app.get("/shop", function (req, res) {
+    fs.readFile('Datas/products.json', (err, data) => {
+		if (err) throw err;
+		res.render('shop', {
+			products: JSON.parse(data)
+		});
+	});
 });
 
 app.listen('8000', 'localhost', () => {
